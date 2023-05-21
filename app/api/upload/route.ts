da02156e30b4e0dd5937d7a6a723b44f50b0efc9
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2023-05-21 13:02:25 +0800

    Refactoring

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,12 +1,18 @@
 import fs from "fs";
+import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   const formData = await req.formData();
-  const files = Array.from(formData.values());
-  for (const file of files) {
-    if (file instanceof Blob) {
-      const buffer = Buffer.from(await file.arrayBuffer());
-      fs.writeFileSync(`public/${file.name}`, buffer);
+  const formDataEntryValues = Array.from(formData.values());
+  for (const formDataEntryValue of formDataEntryValues) {
+    if (
+      typeof formDataEntryValue === "object" &&
+      "arrayBuffer" in formDataEntryValue
+    ) {
+      const fil = formDataEntryValue as unknown as Blob;
+      const buffer = Buffer.from(await fil.arrayBuffer());
+      fs.writeFileSync(`public/${fil.name}`, buffer);
     }
   }
+  return NextResponse.json({ success: true });
 }
